b710ea8e88bfeb95a44a4741d28e960b007e32ba
Rodrigo Caceres <rodrigo.caceres@naranjax.com>
2021-12-29 12:25:56 -0300

    add stubs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-process.env.NODE_ENV = "testing";
+process.env.NODE_ENV = "localhost";
 
 const config = require("config");
 const url = require("url");
 
-const serviceUrl = config.get("services.nosis.API_Consultar.url");
+const serviceUrl = config.get("services.customers.url");
 const urlFormat = url.format(serviceUrl);
 
 console.log(serviceUrl);
